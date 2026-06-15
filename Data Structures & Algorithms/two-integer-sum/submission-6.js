4ba411dd5ce57d8a08feaa442a9fea55a8c706dd
Sean <120344996+Skilzsz@users.noreply.github.com>
2026-06-15 13:14:51 -0700

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+
+
+    // I: array of integers + target integer (Value integers should add up to)
+    // O: array of the indexes that add up to the target
+
+    twoSum(nums, target) {
+
+        
+        for (let i=0;i<nums.length;i++) {
+            let pick = target - nums[i];
+            for (let j=i+1;j<nums.length;j++) {
+                if (pick === nums[j]) {return [i,j]}
+            }
+        }
+        return "Does Not Exist"
+    }
+}
+
+
+
+
+        // let result = 0;
+
+        // for (let i=0;i<nums.length;i++) {
+        //     let remainder = target - nums[i];
+        //     for (let j=i+1;j<nums.length;j++) {
+        //         if (remainder === nums[j]) {return [i,j]}
+        //     }
+        // }
+        // return "Does Not EXIST!"
